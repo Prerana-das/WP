5bed8439395d39af79d9a82c7be2232d7c949cf6
Prerana_das <preranadas97@gmail.com>
2022-07-07 13:04:09 +0600

    custom settings page

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -9,17 +9,39 @@ Author:Prerana
 */
 
 
-add_filter('the_content', 'addToEndOfPost' );
+// ============ Add any string to page/post ============
+// add_filter('the_content', 'addToEndOfPost' );
+// function addToEndOfPost($content){
+//     // return '<p>Dynamic Test string here from our-first-unique-plugin</p>';  without dataa
+//     //return $content. '<p>Dynamic Test string here from our-first-unique-plugin</p>'; // in every  page
 
-function addToEndOfPost($content){
-    // return '<p>Dynamic Test string here from our-first-unique-plugin</p>';  without dataa
-    //return $content. '<p>Dynamic Test string here from our-first-unique-plugin</p>'; // in every  page
+//     // is_page() --show to page // is_single() --> show to blog post
+//     if(is_page() && is_main_query()){
+//         return $content. '<p>Dynamic Test string here from our-first-unique-plugin</p>'; 
+//     }
+
+//     return $content;
+// }
 
 
-    // is_page() --show to page // is_single() --> show to blog post
-    if(is_page() && is_main_query()){
-        return $content. '<p>Dynamic Test string here from our-first-unique-plugin</p>'; 
+class WordCountAndTimePlugin{
+    function __construct(){
+        add_action('admin_menu',array($this,'adminPage'));
     }
+    // ============ Add a new page/link to wordpress admin settting menu ============
     
-    return $content;
+    function adminPage(){
+        // top name, settings right menu, permission /rules, slug/short name(to be unique),function-> html content
+        add_options_page('Word count settings','Custom page Word count','manage_options','word-count-settings-page',array($this,'ourHtml'));
+    }
+
+    function ourHtml(){
+        ?>
+            hello word from the new setting page
+
+        <?php
+    }
+
 }
+$wordCountAndTimePlugin= new WordCountAndTimePlugin();
+
